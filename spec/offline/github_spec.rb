bd4800b3ffe5ff61449ec6a9349a94e188f742a1
Luke Chadwick <me@vertis.io>
2013-11-23 23:33:28 +1100

    mock -> double

--- spec/offline/github_spec.rb
+++ spec/offline/github_spec.rb
@@ -13,26 +13,26 @@ describe Offline::Github do
   
   describe "#repositories" do
     it "should return a list of repositories" do
-      user_response = mock("response", :parsed_response => {})
-      response = mock("response", :parsed_response => [])
+      user_response = double("response", :parsed_response => {})
+      response = double("response", :parsed_response => [])
       response.should_receive(:code).and_return(200)
       Offline::Github.should_receive(:get).with("/users/test/repos?per_page=100").and_return(response)
       Offline::Github.new('user').repositories('test').should == []
     end
     
     it "should return a list of the users repositories" do
-      user_response = mock("response", :parsed_response => {})
+      user_response = double("response", :parsed_response => {})
       user_response.should_receive(:code).and_return(200)
-      response = mock("response", :parsed_response => [])
+      response = double("response", :parsed_response => [])
       Offline::Github.should_receive(:get).with("/user").and_return(user_response)
       Offline::Github.should_receive(:get).with("/user/repos?per_page=100").and_return(response)
       Offline::Github.new('test', 'password').repositories('test').should == []
     end
     
     it "should return only private repositories if requested" do
-      user_response = mock("response")
+      user_response = double("response")
       user_response.should_receive(:code).and_return(200)
-      response = mock("response", :parsed_response => [{"name" => 'public_repo', "private"=>false}, {"name" => 'private_repo', "private"=>true}])
+      response = double("response", :parsed_response => [{"name" => 'public_repo', "private"=>false}, {"name" => 'private_repo', "private"=>true}])
       Offline::Github.should_receive(:get).with("/user").and_return(user_response)
       Offline::Github.should_receive(:get).with("/user/repos?per_page=100").and_return(response)
       repos = Offline::Github.new('test', 'password').repositories('test', :"private-only")
